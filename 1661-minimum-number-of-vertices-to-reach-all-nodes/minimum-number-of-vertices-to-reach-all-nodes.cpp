56cc9495e3b60e5ea78abbcf648eb174bcc84773
bhavesh310 <148660885+bhavesh310@users.noreply.github.com>
2026-08-04 08:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 99.1 MB (51.59%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        //Store indegree of every node
+        vector<int>indegree(n,0);
 
-        // Store indegree of every node
-        vector<int> indegree(n, 0);
+        //Calculate indegree for each node
+        for(auto &edge:edges){
+            int from=edge[0];
+            int to=edge[1];
 
-        // Calculate indegree for each node
-        for (auto &edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-
-            // Incoming edge to 'to'
+            //Incoming edge to 'to'
             indegree[to]++;
         }
 
         vector<int> ans;
 
-        // Nodes having indegree 0 cannot be reached
-        // from any other node, so they must be included.
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0)
-                ans.push_back(i);
-        }
-
+        //Nodes having indegree 0 cannot be reached from any other node
+        //So they must be included as they don't have any incoming edge on them
+        for (int i=0;i<n;i++)
+        if(indegree[i]==0)
+        ans.push_back(i);
+        
         return ans;
     }
 };
