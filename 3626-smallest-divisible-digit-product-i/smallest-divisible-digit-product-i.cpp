354e906b96921a4f208388ab13371a6134e62441
bhavesh310 <148660885+bhavesh310@users.noreply.github.com>
2026-08-06 15:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (73.40%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     // Function to calculate the product of digits of a number
     int digitProduct(int num) {
-        int product = 1;
-
-        // Extract each digit and multiply
-        while (num > 0) {
-            product *= (num % 10);
-            num /= 10;
+        int product=1;
+        //Extract each digit and multiply
+        while(num>0){
+            product*=(num%10);
+            num/=10;
         }
-
         return product;
     }
 
     int smallestNumber(int n, int t) {
-
-        // Keep checking numbers starting from n
+        //Keep checking numbers starting from n
         while (true) {
 
-            // If digit product is divisible by t, return the answer
-            if (digitProduct(n) % t == 0)
-                return n;
+            //If digit product is divisible by t, return the answer
+            if(digitProduct(n)%t==0)
+            return n;
 
-            // Otherwise check the next number
+            //Otherwise check the next number
             n++;
         }
-
-        //return -1; // Unreachable
     }
 };
