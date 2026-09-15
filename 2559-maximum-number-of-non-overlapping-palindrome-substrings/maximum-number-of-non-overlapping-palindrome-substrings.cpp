4fefa0a7b24d279e6ce42ba1b3f0657b29fd5ffb
bhavesh310 <148660885+bhavesh310@users.noreply.github.com>
2026-09-15 21:11:36 +0530

    Time: 138 ms (57.07%) | Memory: 19.6 MB (44.50%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+
+        // pal[i][j] = true if s[i...j] is a palindrome
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s[i] == s[j] && 
+                    (j - i <= 1 || pal[i + 1][j - 1])) {
+                    pal[i][j] = true;
+                }
+            }
+        }
+
+        // dp[i] = maximum palindromes using s[i...n-1]
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            // Skip current character
+            dp[i] = dp[i + 1];
+
+            // Try every palindrome starting at i
+            for (int j = i + k - 1; j < n; j++) {
+                if (pal[i][j]) {
+                    dp[i] = max(dp[i], 1 + dp[j + 1]);
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
